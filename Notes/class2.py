d348068cc02077fbdcd065a6c5812c10326247e4
Kanika Gupta <122543216+kanikaguptakk@users.noreply.github.com>
2024-09-04 04:24:11 +0530

    Add files via upload

---
+++ Notes/class2.py
@@ -0,0 +1,72 @@
+# File Handling- the key function to work with the file is open(), The open() function takes 2 parameters (file name, mode)
+# mode means- what we want to do with the file
+# Read("r"), Write("w"), Append("a"), and Create("x")
+
+#To read a file 
+a=open("C:\\Users\\ftska\\Music\\Encodes\\sample_project_2\\YHills\\sample-1.txt", "r")
+print(a.read(150))   # 150 characters
+# OR
+b=open(r"C:\Users\ftska\Music\Encodes\sample_project_2\YHills\sample-1.txt")
+print(b)
+# OR
+c=open("C:\\Users\\ftska\\Music\\Encodes\\sample_project_2\\YHills\\sample-1.txt")
+print(c.readline(5))
+# OR
+d=open("C:\\Users\\ftska\\Music\\Encodes\\sample_project_2\\YHills\\sample-1.txt")
+print(d.readline(1))
+print(d.readline(1))
+print(d.readline(1))
+d.close()
+# print(d.readline(1))  #file is closed
+
+e=open(r"C:\Users\ftska\Music\Encodes\sample_project_2\YHills\sample-1.txt", "a")
+e.write("Mumma is the best\n")
+e.close()
+e=open(r"C:\Users\ftska\Music\Encodes\sample_project_2\YHills\sample-1.txt", "r")
+print(e.read())
+#Write() will overwrite the existing content, append() will add the content to the last
+f=open("C:\\Users\\ftska\\Music\\Encodes\\sample_project_2\\YHills\\sample-1.txt", "w")
+f.write("She is very nice.")
+f.close()
+f=open("C:\\Users\\ftska\\Music\\Encodes\\sample_project_2\\YHills\\sample-1.txt", "r")
+print(f.read())
+
+#Create()
+# h=open("firstfile.txt", "x")
+# h=open(r"C:\Users\ftska\Music\Encodes\sample_project_2\firstfile.txt", "w")
+# h.write("He is very nice.")
+# h.close()
+# h=open(r"C:\Users\ftska\Music\Encodes\sample_project_2\firstfile.txt", "r")
+# print(h.read())
+
+#Loops and Conditionals in python-ifelse, for
+Weather="Windy" 
+if Weather=="Sunny":   # ==(comparision operator) for comaparision
+    print("we'll go out 50kms")
+elif Weather=="Cloudy":
+    print("We'll go out 4kms")
+elif Weather=="Windy":
+    print("We'll go out 20kms")       
+else:
+    print("no")    
+    
+# for-to iterate over a sequence(list, tuple, set, string) again and again
+w={"Kanika", "Rajeev", "Mamta", "Beenu"}     # Sequence-Set
+for i in w:
+    print(i)    # i-temporary variable
+k=("Vinit", "Pinky", "Shona", "Babu")     #Sequence-Tuple
+for i in k:
+    print(i)    
+j="Girl"
+for i in j:
+    print(i)
+h="Kan ika" 
+for i in h:
+    print(i)
+
+A=input("How is she")
+if A==("good"):
+    print("happy fo her")    
+else:
+    print("will ask her")    # this code will run via running python file not the code. Run code and runnning python differs.
+    
